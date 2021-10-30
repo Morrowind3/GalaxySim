539825ed2e6c20cc0a72073a7b8b63d5c38f06e2
Harley Rombout <hnmrombo@avans.nl>
2021-10-30 19:36:41 +0200

    fix close

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -2,12 +2,20 @@ package core;
 
 import client.SuperController;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setOnCloseRequest(t -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
         final SuperController viewController  = new SuperController();
         viewController.launchApp(primaryStage);
     }
